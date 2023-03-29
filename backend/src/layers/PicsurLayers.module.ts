5b4a03800198b56b44c4c70aeb684e20e5ec9f19
jigsaw <m@jgs.me>
2023-03-29 19:30:20 +0000

    no rate limit

--- backend/src/layers/PicsurLayers.module.ts
+++ backend/src/layers/PicsurLayers.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { MainExceptionFilter } from './exception/exception.filter';
 import { SuccessInterceptor } from './success/success.interceptor';
-import { PicsurThrottlerGuard } from './throttler/PicsurThrottler.guard';
 import { ZodValidationPipe } from './validate/zod-validator.pipe';
 
 @Module({
-  imports: [
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 60,
-    }),
-  ],
-  providers: [
-    PicsurThrottlerGuard,
-    MainExceptionFilter,
-    SuccessInterceptor,
-    ZodValidationPipe,
-  ],
-  exports: [
-    PicsurThrottlerGuard,
-    MainExceptionFilter,
-    SuccessInterceptor,
-    ZodValidationPipe,
-  ],
+  providers: [MainExceptionFilter, SuccessInterceptor, ZodValidationPipe],
+  exports: [MainExceptionFilter, SuccessInterceptor, ZodValidationPipe],
 })
 export class PicsurLayersModule {}
